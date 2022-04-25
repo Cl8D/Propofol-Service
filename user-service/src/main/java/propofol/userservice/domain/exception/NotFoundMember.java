0b70f46f8f492f61768e395fd9fb5b4e77090ea6
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-25 22:43:14 +0900

    (0423) Member 조회 실패 시 예외 처리

--- user-service/src/main/java/propofol/userservice/domain/exception/NotFoundMember.java
+++ user-service/src/main/java/propofol/userservice/domain/exception/NotFoundMember.java
@@ -2,7 +2,7 @@ package propofol.userservice.domain.exception;
 
 import java.util.NoSuchElementException;
 
-// 회원 조회 시 실패했을 때 처리되는 예외
+// 회원 조회 실패 시 처리되는 예외
 public class NotFoundMember extends NoSuchElementException {
     public NotFoundMember() {
         super();
