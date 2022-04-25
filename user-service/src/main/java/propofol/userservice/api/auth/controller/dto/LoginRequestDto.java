10a756f283759d49c239734003cf3873afdfe86f
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-25 22:47:26 +0900

    (0423) 로그인 정보 요청 Dto

--- user-service/src/main/java/propofol/userservice/api/auth/controller/dto/LoginRequestDto.java
+++ user-service/src/main/java/propofol/userservice/api/auth/controller/dto/LoginRequestDto.java
@@ -7,8 +7,7 @@ import lombok.Setter;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
-// 로그인 정보 요청에 대한 dto 추가
-// 로그인 시 이메일-패스워드 입력하니까!
+// 로그인 정보 요청에 대한 dto 추가 (로그인 시 이메일-패스워드 입력)
 @Getter @Setter
 @NoArgsConstructor
 public class LoginRequestDto {
