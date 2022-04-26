d7ab1a5cc64f34d50ea142c9a0283ded1c7de6b7
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-27 02:41:08 +0900

    Chore) Member 권한 수정 (일반 유저, Oauth2 유저 분리 x)

--- user-service/src/main/java/propofol/userservice/domain/member/entity/Authority.java
+++ user-service/src/main/java/propofol/userservice/domain/member/entity/Authority.java
@@ -2,6 +2,6 @@ package propofol.userservice.domain.member.entity;
 
 // Member 권한 설정
 public enum Authority {
-    // admin, 기본 회원가입, oauth 로그인 구분
-    USER_ADMIN, USER_BASIC, USER_SOCIAL
+    // admin, user
+    USER_ADMIN, USER_BASIC
 }
