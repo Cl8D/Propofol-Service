73ee80a385258f0806b3c0a4ec8b3de7714fe2d8
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-27 02:40:38 +0900

    Chore) 프로퍼티 파일 어노테이션으로 읽어올 수 있도록 수정

--- user-service/src/main/java/propofol/userservice/UserServiceApplication.java
+++ user-service/src/main/java/propofol/userservice/UserServiceApplication.java
@@ -4,15 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 // jpaAuditing 활성화
 @EnableJpaAuditing
 // Eureka Client 활성화
 //@EnableEurekaClient
+@ConfigurationPropertiesScan(basePackages = "propofol.userservice.api.common.properties")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -29,5 +32,11 @@ public class UserServiceApplication {
 	@Bean
 	public BCryptPasswordEncoder createEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	// restTemplate Bean 등록
+	@Bean
+	public RestTemplate createRestTemplate() {
+		return new RestTemplate();
 	}
 }
