ad2e90ed6338fe0f827dbd06e7fb8f43822a03ba
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-28 02:39:03 +0900

    :sparkles: Feat) ErrorDto, ErrorDetailDto 추가

---
+++ til-service/src/main/java/propofol/tilservice/api/common/exception/dto/ErrorDto.java
@@ -0,0 +1,19 @@
+package propofol.tilservice.api.common.exception.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Error 정보 저장 Dto (상위 타입)
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ErrorDto {
+    // 에러 상태 코드
+    private Integer status;
+    // 메시지
+    private String message;
+    // 에러가 여러 개일 경우 리스트로 관리
+    private List<ErrorDetailDto> errors = new ArrayList<>();
+}
