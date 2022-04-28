5670c01046cd29b2bdbd41432419efd9925dac18
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-29 01:26:33 +0900

    :sparkles: Feat) 테스트 데이터 비활성화, 게시글 수정 및 삭제 생성자만 가능하도록, 본인 게시글 조회 기능 추가 (컨트롤러)

--- til-service/src/main/java/propofol/tilservice/api/controller/BoardController.java
+++ til-service/src/main/java/propofol/tilservice/api/controller/BoardController.java
@@ -4,21 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
-import org.springframework.security.core.parameters.P;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import propofol.tilservice.api.InitBoardService;
+import propofol.tilservice.api.common.annotation.Token;
 import propofol.tilservice.api.controller.dto.BoardCreateRequestDto;
 import propofol.tilservice.api.controller.dto.BoardListResponseDto;
 import propofol.tilservice.api.controller.dto.BoardResponseDto;
 import propofol.tilservice.api.controller.dto.BoardUpdateRequestDto;
 import propofol.tilservice.domain.board.entity.Board;
-import propofol.tilservice.domain.board.repository.BoardRepository;
 import propofol.tilservice.domain.board.service.BoardService;
 import propofol.tilservice.domain.board.service.dto.BoardDto;
 
-import javax.annotation.PostConstruct;
-import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -30,12 +26,12 @@ public class BoardController {
     private final ModelMapper modelMapper;
 
     // 테스트 데이터 추가
-    private final InitBoardService initBoardService;
-
-    @PostConstruct
-    public void initData() {
-        initBoardService.init();
-    }
+//    private final InitBoardService initBoardService;
+//
+//    @PostConstruct
+//    public void initData() {
+//        initBoardService.init();
+//    }
 
     /*************/
 
@@ -78,12 +74,14 @@ public class BoardController {
 
     // 게시판 글 수정
     // 게시글 번호와 수정 내용이 함께 넘어오게 된다.
+    // 게시글 작성자만 글 수정을 할 수 있도록 @Token 정보를 활용한다.
     @PostMapping("/{boardId}")
-    public String updateBoard(@PathVariable Long boardId, @Validated @RequestBody BoardUpdateRequestDto requestDto) {
+    public String updateBoard(@PathVariable Long boardId,
+                              @RequestBody BoardUpdateRequestDto requestDto,
+                              @Token String memberId) {
         // domain 단의 board entity로 접근할 수 있게 하기 위해서 BoardDto 형태로 변경
         BoardDto boardDto = modelMapper.map(requestDto, BoardDto.class);
-
-        return boardService.updateBoard(boardId, boardDto);
+        return boardService.updateBoard(boardId, boardDto, memberId);
     }
 
     /*******************/
@@ -111,11 +109,29 @@ public class BoardController {
 
     /************************/
 
-    // 게시글 글 삭제
+    // 게시글 글 삭제 - 역시 게시글 작성자만 가능하다.
     @DeleteMapping("/{boardId}")
-    public String deleteBoard(@PathVariable Long boardId) {
-        return boardService.deleteBoard(boardId);
+    public String deleteBoard(@PathVariable Long boardId, @Token String memberId) {
+        return boardService.deleteBoard(boardId, memberId);
     }
 
+
+    /*************/
+
+    // 본인의 게시글만 조회하기
+    // user-service의 FeignClient를 통해 요청이 들어왔을 때 처리되는 메서드
+    // 응답 결과로 총 페이지수, 게시글 수, 게시글 리스트가 담긴 BoardListResponseDto를 넘겨주게 된다. (-> user-service로)
+    /**TODO 본 프로젝트에서 @Token 파라미터 Long->String형으로 변경하기  */
+    @GetMapping("/myBoards")
+    public BoardListResponseDto getPageBoardsByMemberId(@RequestParam Integer page, @Token String memberId) {
+        BoardListResponseDto boardListResponseDto = new BoardListResponseDto();
+        Page<Board> pageBoards = boardService.getPagesByMemberId(page, memberId);
+        boardListResponseDto.setTotalCount(pageBoards.getTotalElements());
+        boardListResponseDto.setTotalPageCount(pageBoards.getTotalPages());
+        pageBoards.forEach(board -> {
+            boardListResponseDto.getBoards().add(modelMapper.map(board, BoardResponseDto.class));
+        });
+        return boardListResponseDto;
+    }
 
 }
