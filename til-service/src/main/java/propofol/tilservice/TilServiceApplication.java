d43897b316b9ade34fb66d6e9ef6684b7a006edc
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-29 01:38:37 +0900

    :sparkles: Feat) Eureka Client, Feign Client 활성화

--- til-service/src/main/java/propofol/tilservice/TilServiceApplication.java
+++ til-service/src/main/java/propofol/tilservice/TilServiceApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +14,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaAuditing
 // 프로퍼티 파일 바인딩
 @ConfigurationPropertiesScan(basePackages = "propofol.tilservice.api.common.properties")
+// feign client 활성화
+@EnableFeignClients(basePackages = "propofol.tilservice.api.feign")
+// eureka client 활성화
+@EnableEurekaClient
 public class TilServiceApplication {
 
 	public static void main(String[] args) {
