112a04a930a32678e77578200911c5ab0e207c5c
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-29 01:26:56 +0900

    :sparkles: Feat) 본인 게시글 조회 기능 추가 (레포지토리)

--- til-service/src/main/java/propofol/tilservice/domain/board/repository/BoardRepository.java
+++ til-service/src/main/java/propofol/tilservice/domain/board/repository/BoardRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import propofol.tilservice.domain.board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -11,5 +13,14 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     // 이때, Query를 직접 작성해줘서 :N으로 연결된 정보를 제외하고
     // 오직 게시글에 대한 정보만 가져올 수 있도록 해준다!
     @Query("select b from Board b")
+    @Transactional(readOnly = true)
     Page<Board> findAll(Pageable pageable);
+
+    // 내가 쓴 글 조회
+    // createdBy(=글 작성자)와 일치하는 게시글 목록을 페이지 형태로 가져오기
+    @Query(countQuery = "select b from Board b where b.createdBy =:createdBy")
+    @Transactional(readOnly = true)
+    Page<Board> findPagesByCreatedBy(Pageable pageable,
+                                     @Param("createdBy") String createdBy);
+
 }
