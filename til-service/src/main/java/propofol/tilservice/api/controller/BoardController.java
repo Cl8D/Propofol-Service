0dd1e316c32062d13c69d512284a5f5367c4ea64
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-30 03:32:58 +0900

    :sparkles: Feat) 게시글 추천, 파일 업로드, 댓글, 대댓글 추가

--- til-service/src/main/java/propofol/tilservice/api/controller/BoardController.java
+++ til-service/src/main/java/propofol/tilservice/api/controller/BoardController.java
@@ -4,16 +4,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import propofol.tilservice.api.common.annotation.Token;
-import propofol.tilservice.api.controller.dto.BoardCreateRequestDto;
-import propofol.tilservice.api.controller.dto.BoardListResponseDto;
-import propofol.tilservice.api.controller.dto.BoardResponseDto;
-import propofol.tilservice.api.controller.dto.BoardUpdateRequestDto;
+import propofol.tilservice.api.common.properties.FileProperties;
+import propofol.tilservice.api.controller.dto.*;
 import propofol.tilservice.domain.board.entity.Board;
 import propofol.tilservice.domain.board.service.BoardService;
+import propofol.tilservice.domain.board.service.CommentService;
+import propofol.tilservice.domain.board.service.RecommendService;
 import propofol.tilservice.domain.board.service.dto.BoardDto;
+import propofol.tilservice.domain.board.service.dto.CommentDto;
+import propofol.tilservice.domain.file.service.ImageService;
+
+import java.util.List;
 
 
 @Slf4j
@@ -24,6 +30,11 @@ public class BoardController {
 
     private final BoardService boardService;
     private final ModelMapper modelMapper;
+    private final FileProperties fileProperties;
+    private final ImageService imageService;
+    private final RecommendService recommendService;
+    private final CommentService commentService;
+
 
     // 테스트 데이터 추가
 //    private final InitBoardService initBoardService;
@@ -38,36 +49,37 @@ public class BoardController {
     // 페이지별로 게시글 가져오기 (쿼리 파라미터로 받아오기. ?page=1)
     // 응답 결과) 총 페이지 수 + 게시글 수 + 게시글 목록 전달하기
     @GetMapping
-    public BoardListResponseDto getPageBoards(@RequestParam Integer page) {
+    public BoardPageResponseDto getPageBoards(@RequestParam Integer page) {
         // 페이지별로 게시글 목록 가져오기
         Page<Board> pageBoards = boardService.getPageBoards(page);
 
         // Dto 형태에 맞춰서 데이터 생성
-        BoardListResponseDto boardListResponseDto = new BoardListResponseDto();
+        BoardPageResponseDto boardPageResponseDto = new BoardPageResponseDto();
 
         // 총 페이지 수
-        boardListResponseDto.setTotalCount(pageBoards.getTotalElements());
+        boardPageResponseDto.setTotalCount(pageBoards.getTotalElements());
         // 총 게시글 수
-        boardListResponseDto.setTotalPageCount(pageBoards.getTotalPages());
+        boardPageResponseDto.setTotalPageCount(pageBoards.getTotalPages());
         // 해당 페이지에 담기는 게시글 목록
         pageBoards.forEach(board -> {
-            boardListResponseDto.getBoards().add(
+            boardPageResponseDto.getBoards().add(
                     // page<Board> 데이터에 있는 board들을 boardResponsetDto에 맞춰서 매핑해주기
                     modelMapper.map(board, BoardResponseDto.class));
         });
 
-        return boardListResponseDto;
+        return boardPageResponseDto;
     }
 
     /**************************/
 
-    // 게시판 글 쓰기
+    // 게시판 글 쓰기 (파일 없이 단순 글쓰기)
     // @NotEmpty 등을 적용하기 위해서 @Validated 붙여주기
     @PostMapping
     public String createBoard(@Validated @RequestBody BoardCreateRequestDto requestDto) {
         // domain 단의 board entity로 접근할 수 있게 하기 위해서 BoardDto 형태로 변경
         BoardDto boardDto = modelMapper.map(requestDto, BoardDto.class);
-        return boardService.saveBoard(boardDto);
+        Board board = boardService.createBoard(boardDto);
+        return boardService.saveBoard(board);
     }
 
     /************************/
@@ -122,15 +134,79 @@ public class BoardController {
     // user-service의 FeignClient를 통해 요청이 들어왔을 때 처리되는 메서드
     // 응답 결과로 총 페이지수, 게시글 수, 게시글 리스트가 담긴 BoardListResponseDto를 넘겨주게 된다. (-> user-service로)
     @GetMapping("/myBoards")
-    public BoardListResponseDto getPageBoardsByMemberId(@RequestParam Integer page, @Token String memberId) {
-        BoardListResponseDto boardListResponseDto = new BoardListResponseDto();
+    public BoardPageResponseDto getPageBoardsByMemberId(@RequestParam Integer page, @Token String memberId) {
+        BoardPageResponseDto boardPageResponseDto = new BoardPageResponseDto();
         Page<Board> pageBoards = boardService.getPagesByMemberId(page, memberId);
-        boardListResponseDto.setTotalCount(pageBoards.getTotalElements());
-        boardListResponseDto.setTotalPageCount(pageBoards.getTotalPages());
+        boardPageResponseDto.setTotalCount(pageBoards.getTotalElements());
+        boardPageResponseDto.setTotalPageCount(pageBoards.getTotalPages());
         pageBoards.forEach(board -> {
-            boardListResponseDto.getBoards().add(modelMapper.map(board, BoardResponseDto.class));
+            boardPageResponseDto.getBoards().add(modelMapper.map(board, BoardResponseDto.class));
         });
-        return boardListResponseDto;
+        return boardPageResponseDto;
+    }
+
+    /*************/
+
+    // 게시글 추천 기능
+    // 게시글의 id를 받아서 해당 게시글의 추천수를 조절해주기
+    // 한 번 클릭하면 추천 업, 한 번 더 클릭하면 추천 다운.
+    @PostMapping("/recommend/{boardId}")
+    public String createRecommend(@PathVariable(value="boardId") Long boardId,
+                                  @Token String memberId) {
+        return recommendService.createRecommend(memberId, boardId);
+    }
+
+    /*********************/
+
+    // 게시글 작성 시 파일을 함께 업로드한다면
+    // 업로드한 파일은 서버에 저장된다.
+    @PostMapping("/files")
+    /** TODO 이 부분 맞게 이해한 것이 맞는지...? */
+    // transactional을 여기서 건 이유
+    // imageService에서 board.addImage(image);로 변경감지가 발생하였으면, 해당 메서드가 종료하면서 flush 되면서
+    // board + image(cascade)의 변경 내용이 함께 db에 반영된다.
+    @Transactional
+    public String createBoardWithFiles(
+            // @RequestParam을 통해서 받는다. 여러 개의 파일이 들어갈 수 있기 때문에
+            @RequestParam("file") List<MultipartFile> files,
+            @RequestParam("title") String title,
+            @RequestParam("content") String content,
+            @RequestParam("open") Boolean open
+    ) {
+
+        // 요청 내용을 바탕으로 dto 생성해주기
+        BoardDto boardDto = new BoardDto();
+        boardDto.setTitle(title);
+        boardDto.setContent(content);
+        boardDto.setOpen(open);
+
+        // 게시글 생성
+        Board board = boardService.createBoard(boardDto);
+        // 게시글 저장
+        boardService.saveBoard(board);
+        // 파일 처리, 업로드할 디렉토리 경로 함께 전달하기
+        imageService.saveBoardFile(fileProperties.getBoardDir(), files, board);
+        return "ok";
+    }
+
+    /*********************/
+
+    // 부모 댓글 (대댓글이 존재하지 않는 가장 첫 댓글에 대해)
+    @PostMapping("/{boardId}/comment")
+    public String createParentComment(@PathVariable(value = "boardId") Long boardId,
+                                      @Validated @RequestBody CommentRequestDto requestDto) {
+        CommentDto commentDto = modelMapper.map(requestDto, CommentDto.class);
+        return commentService.saveParentComment(commentDto, boardId);
+    }
+
+    // 자식 댓글 (대댓글, 하나의 부모 댓글에 대해 여러 자식 댓글이 달린다.)
+    // 에브리타임처럼 부모 댓글 밑에 하위로 여러 대댓글이 있는 (깊이 동일) 형태로 구현
+    @PostMapping("/{boardId}/{parentId}/comment")
+    public String createChildComment(@PathVariable(value = "boardId") Long boardId,
+                                     @PathVariable(value = "parentId") Long parentId,
+                                     @Validated @RequestBody CommentRequestDto requestDto) {
+        CommentDto commentDto = modelMapper.map(requestDto, CommentDto.class);
+        return commentService.saveChildComment(commentDto, boardId, parentId);
     }
 
 }
