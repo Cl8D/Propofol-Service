f9cb9bcdf0f1da352be874e1a95965451536e08a
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-30 03:27:44 +0900

    :sparkles: Feat) 파일 관련 프로퍼티 매핑 클래스 추가

---
+++ til-service/src/main/java/propofol/tilservice/api/common/properties/FileProperties.java
@@ -0,0 +1,19 @@
+package propofol.tilservice.api.common.properties;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+// application.yml에 있는 file에 대한 속성 매핑해주기
+@ConfigurationProperties(prefix="file")
+@ConstructorBinding
+public class FileProperties {
+    private final String profileDir;
+    private final String boardDir;
+
+    public FileProperties(String profileDir, String boardDir) {
+        this.profileDir = profileDir;
+        this.boardDir = boardDir;
+    }
+}
