95eeba6e9908f59bece20678afadd6ad6e11e9ae
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-30 03:20:58 +0900

    :truck: Rename) NotFoundBoard -> NotFoundBoardException

--- til-service/src/main/java/propofol/tilservice/domain/exception/NotFoundBoardException.java
+++ til-service/src/main/java/propofol/tilservice/domain/exception/NotFoundBoardException.java
@@ -3,12 +3,12 @@ package propofol.tilservice.domain.exception;
 import java.util.NoSuchElementException;
 
 // 게시글을 조회할 수 없을 때 처리되는 예외
-public class NotFoundBoard extends NoSuchElementException {
-    public NotFoundBoard() {
+public class NotFoundBoardException extends NoSuchElementException {
+    public NotFoundBoardException() {
         super();
     }
 
-    public NotFoundBoard(String s) {
+    public NotFoundBoardException(String s) {
         super(s);
     }
 }
