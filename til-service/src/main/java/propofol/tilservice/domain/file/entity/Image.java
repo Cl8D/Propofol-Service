4e0f612a28b12eb4fe5f01033fe856936214494e
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-30 03:23:50 +0900

    :sparkles: Feat) 파일 업로드 엔티티 추가

---
+++ til-service/src/main/java/propofol/tilservice/domain/file/entity/Image.java
@@ -0,0 +1,45 @@
+package propofol.tilservice.domain.file.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import propofol.tilservice.domain.board.entity.Board;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Image {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="image_id")
+    private Long id;
+
+    // 업로드된 파일 이름
+    private String uploadFileName;
+
+    // 서버에 저장될 파일 이름
+    private String storeFileName;
+
+    // 타입
+    private String contentType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", updatable = false)
+    private Board board;
+
+    public void addBoard(Board board) {
+        this.board = board;
+    }
+
+    @Builder(builderMethodName = "createImage")
+    public Image(String uploadFileName, String storeFileName, String contentType) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+        this.contentType = contentType;
+    }
+
+
+
+}
