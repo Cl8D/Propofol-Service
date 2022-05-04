4b1544c042611d2c95d894835c67f714c0b2df62
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-04 15:48:10 +0900

    :sparkles: Feat) Exception 추가

--- portfolio-service/src/main/java/propofol/ptfservice/api/common/exception/ExceptionAdviceController.java
+++ portfolio-service/src/main/java/propofol/ptfservice/api/common/exception/ExceptionAdviceController.java
@@ -3,11 +3,15 @@ package propofol.ptfservice.api.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import propofol.ptfservice.api.common.exception.dto.ErrorDto;
+import propofol.ptfservice.domain.exception.NotFoundArchiveException;
+import propofol.ptfservice.domain.exception.NotFoundCareerException;
 import propofol.ptfservice.domain.exception.NotFoundPortfolioException;
+import propofol.ptfservice.domain.exception.NotFoundProjectException;
 
 import javax.validation.ConstraintViolationException;
 
@@ -47,6 +51,46 @@ public class ExceptionAdviceController {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto NotMatchMemberException (NotMatchMemberException e) {
+        ErrorDto errorDto = createError(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return errorDto;
+    }
+
+    /*********************/
+
+    // 아카이브 정보를 찾을 수 없을 때
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto NotFoundArchiveException(NotFoundArchiveException e) {
+        ErrorDto errorDto = createError(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return errorDto;
+    }
+
+    /*********************/
+
+    // 경력 사항 찾을 수 없을 때
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto NotFoundCareerException(NotFoundCareerException e) {
+        ErrorDto errorDto = createError(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return errorDto;
+    }
+
+    /*********************/
+
+    // 프로젝트 찾을 수 없을 때
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto NotFoundProjectException(NotFoundProjectException e) {
+        ErrorDto errorDto = createError(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return errorDto;
+    }
+
+    /*********************/
+
+    // 유효성에 안 맞을 때
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto ValidException (MethodArgumentNotValidException e) {
         ErrorDto errorDto = createError(e.getMessage(), HttpStatus.BAD_REQUEST);
         return errorDto;
     }
