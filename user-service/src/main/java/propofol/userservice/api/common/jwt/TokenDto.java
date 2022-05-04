0d47313ef43d3570cfb3c8c4df5285c82c6e97c5
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-05 00:12:30 +0900

    :bug: Fix) 토큰 만료 기간 삭제

--- user-service/src/main/java/propofol/userservice/api/common/jwt/TokenDto.java
+++ user-service/src/main/java/propofol/userservice/api/common/jwt/TokenDto.java
@@ -11,14 +11,11 @@ public class TokenDto {
     private String type;
     private String accessToken;
     private String refreshToken;
-    // 토큰 만료 기간
-    private Long expirationDate;
 
     @Builder(builderMethodName = "createTokenDto")
-    public TokenDto(String type, String accessToken, String refreshToken, Long expirationDate) {
+    public TokenDto(String type, String accessToken, String refreshToken) {
         this.type = type;
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
-        this.expirationDate = expirationDate;
     }
 }
