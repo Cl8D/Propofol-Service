e565a20513a1d0141ec7ed989deadf94dbeb82d5
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-12 02:14:21 +0900

    :bug: Fix) 생일, 학력, 학점 정보 삭제 (이후 포트폴리오 생성에서 받을 예정)

--- user-service/src/main/java/propofol/userservice/api/member/controller/dto/SaveMemberDto.java
+++ user-service/src/main/java/propofol/userservice/api/member/controller/dto/SaveMemberDto.java
@@ -32,11 +32,4 @@ public class SaveMemberDto {
     @NotEmpty(message = "빈 값을 허용하지 않습니다.")
     @Pattern(regexp = "^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$", message = "올바른 전화번호 형식이 아닙니다.")
     private String phoneNumber;
-
-    @NotEmpty(message = "빈 값을 허용하지 않습니다.")
-    @Pattern(regexp = "^[0-9]{4}-[0-9]{2}-[0-9]{2}$", message = "올바른 날짜 형식이 아닙니다.")
-    private String memberBirth;
-
-    private String degree;
-    private String score;
 }
