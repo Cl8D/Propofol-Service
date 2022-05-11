7e9b7bdf4cc9f8d41541538766fb04793ee7f8ae
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-12 02:14:46 +0900

    :sparkles: Feat) 닉네임 회원 조회 추가

--- user-service/src/main/java/propofol/userservice/domain/member/repository/MemberRepository.java
+++ user-service/src/main/java/propofol/userservice/domain/member/repository/MemberRepository.java
@@ -10,13 +10,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // 이메일을 통해 멤버 조회
     Optional<Member> findByEmail(String email);
 
-    // 닉네임을 통해 회원 조회 (중복 체크를 위함)
-    Member findDuplicateByNickname(String nickname);
-    // 이메일을 통해 회원 조회 (중복 체크를 위함)
-    Member findDuplicateByEmail(String email);
-
-    // 이메일을 통해 회원 존재 여부 확인 (Oauth 사용자)
+    // 이메일을 통해 회원 존재 여부 확인 (Oauth 사용자) + 중복 체크
     Member findExistByEmail(String email);
+
+    // 닉네임을 통해 회원 존재 여부 확인 (중복 체크)
+    Member findExistByNickname(String nickname);
 
     // refreshToken으로 멤버 찾기
     Optional<Member> findByRefreshToken(String refreshToken);
