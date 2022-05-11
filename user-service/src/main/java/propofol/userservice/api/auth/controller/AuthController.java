eea2f0ea7ed9c7f567672688c4f291849c500862
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-12 02:16:14 +0900

    :sparkles: Feat) 이메일, 닉네임 중복 체크 추가

--- user-service/src/main/java/propofol/userservice/api/auth/controller/AuthController.java
+++ user-service/src/main/java/propofol/userservice/api/auth/controller/AuthController.java
@@ -10,10 +10,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import propofol.userservice.api.auth.controller.dto.LoginRequestDto;
-import propofol.userservice.api.auth.controller.dto.ResponseDto;
-import propofol.userservice.api.auth.controller.dto.UpdatePasswordRequestDto;
+import propofol.userservice.api.auth.controller.dto.*;
 import propofol.userservice.api.auth.service.AuthService;
+import propofol.userservice.api.common.exception.DuplicateEmailException;
+import propofol.userservice.api.common.exception.DuplicateNicknameException;
 import propofol.userservice.api.common.exception.dto.ErrorDetailDto;
 import propofol.userservice.api.common.exception.dto.ErrorDto;
 import propofol.userservice.api.common.jwt.JwtProvider;
@@ -39,6 +39,23 @@ public class AuthController {
     private final BCryptPasswordEncoder encoder;
     private final JwtProvider jwtProvider;
 
+
+    /****************/
+    // 이메일, 닉네임 중복 체크 분리
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDto duplicateEmailException(DuplicateEmailException e){
+        return new ResponseDto(HttpStatus.BAD_REQUEST.value(), "fail", "중복 오류", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseDto duplicateNicknameException(DuplicateNicknameException e){
+        return new ResponseDto(HttpStatus.BAD_REQUEST.value(), "fail", "중복 오류", e.getMessage());
+    }
+
+    /***************/
+
     // 로그인
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
@@ -63,62 +80,50 @@ public class AuthController {
     @ResponseStatus(HttpStatus.CREATED)
     // @validated를 통해 원하는 속성에 대해서만 유효성 검사 가능
     // requestBody로 회원 가입 시 해당 멤버 정보 저장
-    public ResponseDto saveMember(@Validated @RequestBody SaveMemberDto saveMemberDto, HttpServletResponse response){
-        ErrorDto errorDto = new ErrorDto();
-
-        // 이메일, 닉네임에 대한 중복 체크
-        checkDuplicate(saveMemberDto, errorDto);
-
-        // 만약 중복이 존재한다면
-        if(errorDto.getErrors().size() != 0){
-            // errorDto 객체를 통해 오류 정보 저장
-            errorDto.setErrorMessage("중복 오류!");
-            // status 상태 설정
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), "fail", "회원 저장 실패!", errorDto);
-        }
-
-        // 회원가입 받은 string 형 생년월일 정보를 날짜 형태로 변경
-        String birth = saveMemberDto.getMemberBirth();
-        LocalDate date = LocalDate.parse(birth, DateTimeFormatter.ISO_DATE);
-
-        // member 객체 생성
-        Member member = createMember(saveMemberDto, date);
-
+    public ResponseDto saveMember(@Validated @RequestBody SaveMemberDto saveMemberDto){
+        Member member = createMember(saveMemberDto);
         // 회원 가입 진행
         memberService.saveMember(member);
-
         return new ResponseDto<>(HttpStatus.CREATED.value(), "success", "회원 저장 성공!", "ok");
     }
 
-    private Member createMember(SaveMemberDto saveMemberDto, LocalDate date) {
+    // 이메일 중복 체크
+    @PostMapping("/email")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto checkDuplicateEmail(@Validated @RequestBody EmailRequestDto requestDto) {
+        if(memberService.isExistByEmail(requestDto.getEmail())) {
+            throw new DuplicateEmailException("이메일 중복");
+        }
+
+        return new ResponseDto(HttpStatus.OK.value(), "success", "이메일 중복 없음", "ok");
+    }
+
+    // 닉네임 중복 체크
+    @PostMapping("/nickname")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDto checkDuplicateNickname(@Validated @RequestBody NicknameRequestDto requestDto){
+        if(memberService.isExistByNickname(requestDto.getNickname())){
+            throw new DuplicateNicknameException("닉네임 중복");
+        }
+        return new ResponseDto(HttpStatus.OK.value(), "success", "닉네임 중복 없음", "ok");
+    }
+
+
+    /***************************/
+
+    private Member createMember(SaveMemberDto saveMemberDto) {
         Member member = Member.createMember()
                 .email(saveMemberDto.getEmail())
                 // 비밀번호 설정 시 암호화해서 넣기
                 .password(encoder.encode(saveMemberDto.getPassword()))
                 .nickname(saveMemberDto.getNickname())
                 .username(saveMemberDto.getUsername())
-                .birth(date)
-                .degree(saveMemberDto.getDegree())
                 .phoneNumber(saveMemberDto.getPhoneNumber())
-                .score(saveMemberDto.getScore())
                 .authority(Authority.ROLE_USER)
                 .build();
         return member;
     }
 
-    // 중복 체크
-    private void checkDuplicate(SaveMemberDto saveMemberDto, ErrorDto errorDto) {
-        // 이메일 중복 체크
-        if(memberService.checkDuplicateByEmail(saveMemberDto.getEmail())){
-            errorDto.getErrors().add(new ErrorDetailDto("Email", "중복 오류"));
-        }
-
-        // 닉네임 중복 체크
-        if(memberService.checkDuplicateByNickname(saveMemberDto.getNickname())){
-            errorDto.getErrors().add(new ErrorDetailDto("Nickname", "중복 오류"));
-        }
-    }
 
     /********************/
 
