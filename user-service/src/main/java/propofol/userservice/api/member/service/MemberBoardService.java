fe9fdf1a5103b7eea49c2944dd40e5602c9f5067
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-15 19:01:20 +0900

    :bug: Fix) 응답 결과 없을 경우 null 처리

--- user-service/src/main/java/propofol/userservice/api/member/service/MemberBoardService.java
+++ user-service/src/main/java/propofol/userservice/api/member/service/MemberBoardService.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import propofol.userservice.api.feign.TilServiceFeignClient;
-import propofol.userservice.api.member.controller.dto.MemberBoardsResponseDto;
+import propofol.userservice.api.member.controller.dto.board.MemberBoardsResponseDto;
 
 // user-service <-> til-service 간 통신을 위해 사용되는 서비스 계층.
 @Service
@@ -17,6 +17,6 @@ public class MemberBoardService {
     // 컨트롤러단에서 넘겨준 page 정보와 token 정보를 이용해 feignClient 호출
     public MemberBoardsResponseDto getMyBoards(Integer page, String token) {
         // 응답 결과로 총 페이지수, 게시글수, 게시글 리스트가 담긴 Dto를 return 받게 된다.
-        return tilServiceFeignClient.getMyBoards(token, page);
+        return tilServiceFeignClient.getMyBoards(token, page).orElse(null);
     }
 }
