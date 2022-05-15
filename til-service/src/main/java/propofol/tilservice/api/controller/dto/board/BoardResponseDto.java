6a1e2e7c1e911c9db4c9416ddc49d2d6278864aa
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-15 16:58:17 +0900

    :bug: Fix) 게시글 목록에 띄워줄 때 사용되는 응답 DTO (필드 추가)

--- til-service/src/main/java/propofol/tilservice/api/controller/dto/board/BoardResponseDto.java
+++ til-service/src/main/java/propofol/tilservice/api/controller/dto/board/BoardResponseDto.java
@@ -4,14 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// 하나의 게시글에 대한 정보를 담는 Dto
+import java.time.LocalDateTime;
+
+// 게시글 목록을 띄워줄 때 보여주는 DTO
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardResponseDto {
-    private Long boardUUID;
+    // 게시글 id
+    private Long id;
+    // 제목
     private String title;
+    // 내용
     private String content;
+    // 첨부 이미지 중 가장 처음 이미지 (썸네일)의 바이트
+    private byte[] imageBytes;
+    // 이미지 유형
+    private String imageType;
+    // 추천 수
     private Integer recommend;
+    // 댓글 수
+    private Integer commentCount;
+    // 공개 여부
     private Boolean open;
+    // 게시글 생성일
+    private LocalDateTime createdDate;
 }
