998cc09820c375e5e9c514ba3f747f4263c3cbf9
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-15 16:54:44 +0900

    :sparkles: Feat) 댓글 응답 DTO에 댓글 작성 시간 추가

--- til-service/src/main/java/propofol/tilservice/api/controller/dto/comment/CommentResponseDto.java
+++ til-service/src/main/java/propofol/tilservice/api/controller/dto/comment/CommentResponseDto.java
@@ -3,6 +3,8 @@ package propofol.tilservice.api.controller.dto.comment;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 // 하나의 댓글에 대한 응답 DTO
 @Data
 @NoArgsConstructor
@@ -11,11 +13,13 @@ public class CommentResponseDto {
     private String nickname;
     private String content;
     private Long groupId;
+    private LocalDateTime createdDate;
 
-    public CommentResponseDto(Long id, String nickname, String content, Long groupId) {
+    public CommentResponseDto(Long id, String nickname, String content, Long groupId, LocalDateTime createdDate) {
         this.id = id;
         this.nickname = nickname;
         this.content = content;
         this.groupId = groupId;
+        this.createdDate = createdDate;
     }
 }
